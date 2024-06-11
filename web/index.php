52e073418a64d53ee4cc668181f2ce5478b78277
Felipe Valencio <fgv.valencio@gmail.com>
2024-06-11 19:06:37 -0300

    update index

--- web/index.php
+++ web/index.php
@@ -1,1 +1,8 @@
-<?php include_once("index.html"); 
+<?php
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
+// Serve the requested resource as-is.
+include_once("index.html");
+?>
